b166e2c7762e64935a37aae8c00aec23df976864
0xbotted <0xbotted@gmail.com>
2025-05-14 13:20:55 +0530

    updated robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,11 +3,18 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: `*`,
-      allow: `/`,
-      disallow: `/private/`,
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/private/", "/admin/"],
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+        crawlDelay: 10,
+      },
+    ],
     sitemap: `${CONFIG.BASE_URL}/sitemap.xml`,
   };
 }
